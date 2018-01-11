7ac3f6c036576ccdef7baff6158239092b3affc0
FBemf <felipe.m.p.bemfica@gmail.com>
2018-01-11 02:39:40 -0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,17 +1,18 @@
 # RSA encryption
 
 import mr
+import eu
 import random
 
 # Miller-Rabin Tester
 def main():
-    print(getPrime(1024))
+    print(makeKeys(1024))
 
 # Prime Finder
 
 # Find a prime between a and b
 def getPrime(bits):
-    getPrimeRange(2**(bits-1), 2**bits)
+    return getPrimeRange(2**(bits-1), 2**bits)
 
 def getPrimeRange(a, b):
     r = random.SystemRandom()
@@ -27,14 +28,33 @@ def getPrimeRange(a, b):
     return 0
 
 # Totient Getter
-def eulerPhi(a, b):
+def carmichael(a, b):
     #assumes a and b are prime
-    return (a-1)*(b-1)
+    return eu.lcm((a-1), (b-1))
 
 # 65537
 ENCRYPTION_BASE = 65537
 
+# Create Keys
+def makeKeys(b):
+    # Makes a public modulus n and a private key d
+    p=0
+    q=0
+    while not p:
+        p = getPrime(1024)
+    while not q:
+        q = getPrime(1024)
+
+    n = p * q
+    y = carmichael(p, q)
+    e = ENCRYPTION_BASE
+
+    d = abs(eu.eux(e, y)[0])
+
+    return (n, d)
+
 # Encryptor
+
 # Decryptor
 # Text-to-number and back
 
